ee504551a73c1f419926106105eae72bb25ca958
Maruf Siddiqui <maruf.siddiqui@shore.com>
2017-08-19 13:34:22 +0200

    Initial client app setup

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ require("./routes/authRoutes")(app);
 
 app.get("/", (req, res) => res.send("Hello"));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.info("Server is running on port ", port);
 });
